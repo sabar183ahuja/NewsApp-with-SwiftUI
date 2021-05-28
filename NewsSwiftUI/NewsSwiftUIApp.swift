0e5b097b166e4c342c07e6e00133430f24c31894
Sabar Ahuja <sabar.noida@gmail.com>
2021-05-28 11:53:09 +0530

    Initial Commit

---
+++ NewsSwiftUI/NewsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsSwiftUIApp.swift
+//  NewsSwiftUI
+//
+//  Created by Sabar Ahuja on 2021-05-28.
+//
+
+import SwiftUI
+
+@main
+struct NewsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
